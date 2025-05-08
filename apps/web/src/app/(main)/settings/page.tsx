7e16893aa56ff8aa7857bf7ebdbc34d2114b1b7a
Abdul Irfan <unknown7k7k@gmail.com>
2025-05-08 07:33:50 +0530

    feat(web): add location fields to account setting page

--- apps/web/src/app/(main)/settings/page.tsx
+++ apps/web/src/app/(main)/settings/page.tsx
@@ -1,7 +1,17 @@
 import { Button } from '@/components/ui/button';
 import { TextField } from '@/components/ui/form/Inputs';
 import { EmailField } from '@/components/ui/form/Inputs/EmailField';
+import { Label } from '@/components/ui/form/Inputs/Label';
 import { PasswordField } from '@/components/ui/form/Inputs/PasswordField';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import React from 'react';
 
 const SettingPage = () => {
@@ -15,6 +25,51 @@ const SettingPage = () => {
           <EmailField label="Email" />
           <PasswordField label="Password" />
           <TextField label="Phone Number" type="text" />
+
+          <div className="flex flex-col gap-6 mt-6">
+            <span>Location</span>
+            <div className="flex flex-col gap-1.5">
+              <Label>Country/Region</Label>
+              <Select>
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Select a Location" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Cities</SelectLabel>
+                    <SelectItem value="bangalore">Bangalore</SelectItem>
+                    <SelectItem value="mangalore">Mangalore</SelectItem>
+                    <SelectItem value="delhi">Delhi</SelectItem>
+                    <SelectItem value="mumbai">Mumbai</SelectItem>
+                    <SelectItem value="chennai">Chennai</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <Label>State</Label>
+              <Select>
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Select a State" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>States</SelectLabel>
+                    <SelectItem value="karnataka">Karnataka</SelectItem>
+                    <SelectItem value="kerala">Kerala</SelectItem>
+                    <SelectItem value="tamilnadu">Tamil Nadu</SelectItem>
+                    <SelectItem value="telangana">Telangana</SelectItem>
+                    <SelectItem value="andhrapradesh">
+                      Andhra Pradesh
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <TextField label="Town/City" type="text" />
+            <TextField label="Postcode" type="text" />
+          </div>
 
           <div className="flex justify-between items-center">
             <span>Delete Account</span>
