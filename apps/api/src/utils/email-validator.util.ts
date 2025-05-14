77c97afd9bfac10940b7bf51ab70f34f8fc65246
Abdul Irfan <unknown7k7k@gmail.com>
2025-05-14 10:21:19 +0530

    feat(api):  add development environment check to email validation utility

--- apps/api/src/utils/email-validator.util.ts
+++ apps/api/src/utils/email-validator.util.ts
@@ -1,6 +1,9 @@
 import validate from 'deep-email-validator';
 
+import { NODE_ENV } from '@/config';
+
 export const checkEmailExists = async (email: string): Promise<boolean> => {
+  if (NODE_ENV === 'development') return true;
   const { valid } = await validate({ email });
   return valid;
 };
