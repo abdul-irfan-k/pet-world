b7302c30242d7b1ab2934b7cf46ac244f89550ad
Abdul Irfan <unknown7k7k@gmail.com>
2025-05-24 05:56:24 +0530

    fix(api): update cookie configurations

--- apps/api/src/controller/auth.controller.ts
+++ apps/api/src/controller/auth.controller.ts
@@ -56,13 +56,13 @@ export class AuthController implements IAuthController {
       res.cookie('accessToken', result.accessToken, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: 'lax',
+        sameSite: isProduction ? 'none' : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       res.cookie('refreshToken', result.refreshToken, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: 'lax',
+        sameSite: isProduction ? 'none' : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
