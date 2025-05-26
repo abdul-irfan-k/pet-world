cb8c4022067666a2a39e96afc1bacb2ddbc84e37
Abdul Irfan <unknown7k7k@gmail.com>
2025-05-26 17:20:29 +0530

    feat(api): add redis support and update tokenhandlign

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -1,5 +1,7 @@
 export * from './cloudinary.config';
 export * from './env.config';
 export * from './logger.config';
+export * from './multer.config';
 export * from './nodemailer.config';
 export * from './prisma.config';
+export * from './redis.config';
--- apps/web/src/lib/api-client/apiClient.ts
+++ apps/web/src/lib/api-client/apiClient.ts
@@ -5,3 +5,27 @@ export const apiClient = axios.create({
     process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001/api/v1/',
   withCredentials: true,
 });
+
+apiClient.interceptors.response.use(
+  response => response,
+  async error => {
+    if (
+      error.response.statusCode === 401 &&
+      error.response.data.message === 'ACCESS_TOKEN_EXPIRED'
+    ) {
+      try {
+        const originalRequest = error.config;
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/refresh-token`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+        return apiClient(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+  },
+);
