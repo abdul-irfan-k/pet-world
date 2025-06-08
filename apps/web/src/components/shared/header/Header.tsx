831f892a5ea47728ef1334298e4756c9c9115466
Abdul Irfan <unknown7k7k@gmail.com>
2025-06-08 17:20:22 +0530

    fix(web): resolve nextjs build error caused eslint un-used vars

--- apps/web/src/components/shared/header/Header.tsx
+++ apps/web/src/components/shared/header/Header.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { Search, ShoppingBag, User2, Settings, Heart, LogOut } from 'lucide-react';
+import { ShoppingBag, User2, Settings, Heart, LogOut } from 'lucide-react';
 
 import { Button } from '../../ui/button';
 import { HeaderSearch } from '../search/HeaderSearch';
