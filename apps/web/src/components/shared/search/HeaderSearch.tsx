f51a29ab06f6e32d1488595278d5ff0f1120464a
Abdul Irfan <unknown7k7k@gmail.com>
2025-06-09 19:24:05 +0530

    feat(web): update header search

--- apps/web/src/components/shared/search/HeaderSearch.tsx
+++ apps/web/src/components/shared/search/HeaderSearch.tsx
@@ -9,7 +9,6 @@ import { AdoptionEndTab, AdoptionStartTab, LocationTab, SpeciesTab } from './tab
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/form/inputs';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export type TabKeys = 'where' | 'adoptionStart' | 'adoptionEnd' | 'species' | undefined;
 
@@ -46,82 +45,108 @@ const HeaderSearch = () => {
 
   return (
     <div className="mx-auto flex w-screen items-end justify-center" ref={scrollRef}>
-      <Tabs
-        defaultValue="where"
-        value={selectedTab}
-        //eslint-disable-next-line
-        //@ts-ignore
-        onValueChange={setSelectedTab}
+      <motion.div
+        className={'w-215 relative flex min-w-fit justify-center rounded-full bg-[#ebebeb98] p-1'}
+        style={{ translateY, height, width }}
+        transition={{ type: 'spring', stiffness: 80, damping: 20 }}
       >
-        <motion.div
-          className="w-215 flex justify-center rounded-full bg-gray-100"
-          style={{ width, translateY, height }}
-          transition={{ type: 'spring', stiffness: 80, damping: 20 }}
+        <div
+          className="flex-1/2 relative flex h-full min-w-fit cursor-pointer flex-col items-start justify-center rounded-full px-8"
+          onClick={() => setSelectedTab('where')}
         >
-          <TabsList className="h-full w-full rounded-full">
-            <TabsTrigger
-              className="flex-1/2 flex w-full flex-col items-start justify-center rounded-full px-8"
-              value="where"
-            >
-              Where
-              <motion.div className="text-gray-400" style={{ display: show ? 'block' : 'none' }}>
-                <Input
-                  placeholder="Search locations..."
-                  className="ml-0 w-full"
-                  style={{ display: show ? 'block' : 'none' }}
-                />
-              </motion.div>
-            </TabsTrigger>
+          <span className="z-70 text-sm font-medium">Where</span>
+          <motion.div className="z-70 text-gray-700" style={{ display: show ? 'block' : 'none' }}>
+            <Input
+              placeholder="Search locations..."
+              className="ml-0 w-full"
+              style={{ display: show ? 'block' : 'none' }}
+            />
+          </motion.div>
+          {selectedTab === 'where' && (
+            <motion.div
+              layoutId="active-tab"
+              className="z-60 absolute inset-0 bg-white"
+              style={{ borderRadius: 1000 }}
+            />
+          )}
+        </div>
 
-            <TabsTrigger
-              className="flex-1/4 flex flex-col items-start justify-center rounded-full px-8"
-              value="adoptionStart"
-            >
-              Start Date
-              <motion.div className="text-gray-400" style={{ display: show ? 'block' : 'none' }}>
-                Select Start Date
-              </motion.div>
-            </TabsTrigger>
+        <div
+          className="flex-1/3 relative flex h-full min-w-fit cursor-pointer flex-col items-start justify-center rounded-full px-8"
+          onClick={() => setSelectedTab('adoptionStart')}
+        >
+          <span className="z-70 text-sm font-medium">Start Date</span>
+          <motion.div className="z-70 text-gray-700" style={{ display: show ? 'block' : 'none' }}>
+            Select Start Date
+          </motion.div>
+          {selectedTab === 'adoptionStart' && (
+            <motion.div
+              layoutId="active-tab"
+              className="z-60 absolute inset-0 bg-white"
+              style={{ borderRadius: 1000 }}
+            />
+          )}
+        </div>
 
-            <TabsTrigger
-              className="flex-1/4 flex flex-col items-start justify-center rounded-full px-8"
-              value="adoptionEnd"
-            >
-              End Date
-              <motion.div className="text-gray-400" style={{ display: show ? 'block' : 'none' }}>
-                Select End Date
-              </motion.div>
-            </TabsTrigger>
+        <div
+          className="flex-1/3 relative flex h-full min-w-fit cursor-pointer flex-col items-start justify-center rounded-full px-8"
+          onClick={() => setSelectedTab('adoptionEnd')}
+        >
+          <span className="z-70 text-sm font-medium">End Date</span>
+          <motion.div className="z-70 text-gray-700" style={{ display: show ? 'block' : 'none' }}>
+            Select End Date
+          </motion.div>
+          {selectedTab === 'adoptionEnd' && (
+            <motion.div
+              layoutId="active-tab"
+              className="z-60 absolute inset-0 bg-white"
+              style={{ borderRadius: 1000 }}
+            />
+          )}
+        </div>
+        <div
+          className="flex-1/2 relative flex h-full min-w-fit cursor-pointer flex-col items-start justify-center rounded-full px-8"
+          onClick={() => setSelectedTab('species')}
+        >
+          <span className="z-70 text-sm font-medium">End Date</span>
+          <motion.div className="z-70 text-gray-700" style={{ display: show ? 'block' : 'none' }}>
+            Select End Date
+          </motion.div>
+          {selectedTab === 'species' && (
+            <motion.div
+              layoutId="active-tab"
+              className="z-60 absolute inset-0 bg-white"
+              style={{ borderRadius: 1000 }}
+            />
+          )}
+        </div>
+        {selectedTab === undefined && (
+          <motion.div layoutId="active-tab" className="z-60 absolute inset-1 bg-white" style={{ borderRadius: 1000 }} />
+        )}
 
-            <TabsTrigger
-              className="flex-1/2 relative flex flex-col items-start justify-center rounded-full px-8"
-              value="species"
-            >
-              <div className="pr-5">Species</div>
-              <motion.div className="text-gray-400" style={{ display: show ? 'block' : 'none' }}>
+        {/* <div className="pr-5">Species</div> */}
+        {/* <motion.div className="z-70 text-gray-700" style={{ display: show ? 'block' : 'none' }}>
                 Select Species
-              </motion.div>
-              <div className="absolute right-0">
+              </motion.div> */}
+        {/* <div className="absolute right-0">
                 <Button rounded className="h-full p-4">
                   <Search className="h-4 w-4" />
                   <motion.div className="text-white" style={{ display: show ? 'block' : 'none' }}>
                     <span className="text-sm font-semibold">Search</span>
                   </motion.div>
                 </Button>
-              </div>
-            </TabsTrigger>
-          </TabsList>
-        </motion.div>
+              </div> */}
+        {/* </TabsTrigger> */}
+      </motion.div>
 
-        <div className="mt-45 absolute z-20 min-h-[150px]">
-          {selectedTab === 'where' && <LocationTab />}
-          {selectedTab === 'adoptionStart' && <AdoptionStartTab dateRange={dateRange} setDateRange={setDateRange} />}
-          {selectedTab === 'adoptionEnd' && <AdoptionEndTab dateRange={dateRange} setDateRange={setDateRange} />}
-          {selectedTab === 'species' && (
-            <SpeciesTab selectedSpecies={selectedSpecies} onSpeciesChange={handleSpeciesChange} />
-          )}
-        </div>
-      </Tabs>
+      {/* <div className="mt-45 absolute z-20 min-h-[150px]">
+        {selectedTab === 'where' && <LocationTab />}
+        {selectedTab === 'adoptionStart' && <AdoptionStartTab dateRange={dateRange} setDateRange={setDateRange} />}
+        {selectedTab === 'adoptionEnd' && <AdoptionEndTab dateRange={dateRange} setDateRange={setDateRange} />}
+        {selectedTab === 'species' && (
+          <SpeciesTab selectedSpecies={selectedSpecies} onSpeciesChange={handleSpeciesChange} />
+        )}
+      </div> */}
     </div>
   );
 };
