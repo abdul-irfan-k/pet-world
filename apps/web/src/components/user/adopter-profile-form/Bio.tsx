16f2cb342d06b1aa180bb4a3c9f3a37d5e20f527
Abdul Irfan <unknown7k7k@gmail.com>
2025-06-21 16:59:32 +0530

    feat(web): integrate react-hook-form for bio editor component

--- apps/web/src/components/user/adopter-profile-form/Bio.tsx
+++ apps/web/src/components/user/adopter-profile-form/Bio.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 
 import dynamic from 'next/dynamic';
 
+import { useFormContext } from 'react-hook-form';
+
 const Editor = dynamic(() => import('@/components/ui/editor').then(mod => mod.Editor), {
   ssr: false,
   loading: () => <div>Loading editor...</div>,
 });
 const Bio = () => {
-  const [value, setValue] = useState('');
+  const { watch, setValue } = useFormContext();
   return (
     <div className="w-[45vw] space-y-6 rounded-lg border border-gray-300 bg-gray-50 p-10">
       <p className="text-muted-foreground text-sm">
@@ -15,7 +17,15 @@ const Bio = () => {
       </p>
 
       <div className="h-min-[80vh] space-y-4">
-        <Editor value={value} onChange={setValue} className="h-full" style={{ height: '50vh' }} />
+        <Editor
+          value={watch('overview.bio')}
+          onChange={value => {
+            console.log(value);
+            setValue('overview.bio', value);
+          }}
+          className="h-full"
+          style={{ height: '50vh' }}
+        />
       </div>
     </div>
   );
