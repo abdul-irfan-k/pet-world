4215d16c78e6095b8ff9bf6cce16cf9b405216df
Abdul Irfan <unknown7k7k@gmail.com>
2025-06-30 19:17:00 +0530

    feat(web): implement public layout and update social media icons in footer

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Footer, Header } from '@/components/shared';
 
-export default function PetsLayout({ children }: { children: React.ReactNode }) {
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
       <Header />
